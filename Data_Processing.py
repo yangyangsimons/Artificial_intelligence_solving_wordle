7c76885875addf4137e3f95ec2e39860986232f6
shuati <ouyangyly@gmail.com>
2022-07-09 23:58:46 +0100

    wordle simulation finished

---
+++ Data_Processing.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from string import ascii_lowercase
+
+data1 = pd.read_csv("/Users/lynnyang/work/wordle/data/allowed_words.txt",header=None)
+data2 = pd.read_csv("/Users/lynnyang/work/wordle/data/2021_2022_answer_list.csv")
+testWords = list(data2["Answer"])
+allWords = list(data1[0])
+
+
+valid_word_list = []
+sorted_word_list = []
+for word in allWords:
+    if (len(word)== 5):
+# if the word has an uppercase letter, then ignore the whole word.
+        if(any(letter.isupper() for letter in word)):
+            continue
+        else:
+# sort the word in alphabet order, for example "sample" will sorted as "aelmps", this is used in the later function;
+            sorted_word = "".join(sorted(word))
+# add all the word length equals 5 into the valid_word_list and the sorted word into sorted_word_list;
+            valid_word_list.append(word)
+            sorted_word_list.append(sorted_word)
---
+++ Wordle_Simulation.py
@@ -0,0 +1,32 @@
+import Data_Processing as data
+import termcolor as colored
+import random
+targetWord = random.choice(data.testWords)  
+targetWord
+
+def __validWord__(answer):
+    if(len(answer) > 5):
+        print("5 letters only")
+        return False
+    else:
+        if(answer in data.allWords):
+            return True
+        else:
+            print("This is not a valid word")
+            return False
+
+def __feedback__(userWord, targetWord):
+    validWord = __validWord__(userWord)
+    color = []
+    if(validWord):
+    
+        for i in range(len(targetWord)):
+            # check if this letter is in target word
+            if(userWord[i] in targetWord):
+                if (userWord[i] == targetWord[i]):
+                    color.append("green")
+                else:
+                    color.append("yellow")
+            else:
+                color.append("grey")
+    return color
