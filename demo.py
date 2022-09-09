3adb15678e15056ca414f0b35ae7c882e798bd86
shuati <ouyangyly@gmail.com>
2022-09-09 12:33:17 +0100

    delete gnatt chart and java version wordle simulation

--- demo.py
+++ demo.py
@@ -71,7 +71,7 @@ def __Set__New__Word__List__(selectedWord,feedback,word_list):
             if(word.find(letter) == -1):
                 letter_status.append(True)
             else:
-                if(letter in yellow_dic.values() or letter in green_dic.values() and word[index] != letter):
+                if((letter in yellow_dic.values()) or (letter in green_dic.values()) and (word[index] != letter)):
                     letter_status.append(True)
                 else:
                     letter_status.append(False)
